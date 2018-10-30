f2b46d67773010df18dbf34b9c3ab151784c83fc
Ankit Kumar <ankitkr2294@gmail.com>
2018-10-30 06:36:59 +0530

    Create Graph.java
    Graph Implementation using Adjacency List.

---
+++ Graph/Graph.java
@@ -0,0 +1,56 @@
+package com.ankit.graph;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+public class Graph {
+
+	ArrayList<Integer>[] list;
+	int vert;
+	@SuppressWarnings("unchecked")
+	public Graph(int vertices){
+		this.vert = vertices;
+		list = new ArrayList[vertices];
+		for(int i=0;i<vertices;i++){
+			list[i] = new ArrayList<Integer>();
+		}
+	}
+	public void addEdge(int src,int dest){
+		list[src].add(dest);
+		list[dest].add(src);
+	}
+	public void printGraph(){
+		int index = 0;
+		
+		for(ArrayList<Integer> eachList : list){
+			System.out.print(index + ":");
+			for(Integer i : eachList){
+				System.out.print(i+"-->");
+			}
+			index++;
+			System.out.println();
+		}
+	}
+	public static void main(String[] args) throws Exception{
+		InputStreamReader reader = new InputStreamReader(System.in);
+		BufferedReader buff = new BufferedReader(reader);
+		
+		String data [] = buff.readLine().split(" ");
+		int vert = Integer.parseInt(data[0]);
+		int edges = Integer.parseInt(data[1]);
+		Graph g = new Graph(vert+1);
+		for(int i=0;i<edges;i++){
+			data = buff.readLine().split(" ");
+			int src = Integer.parseInt(data[0]);
+			int dest = Integer.parseInt(data[1]);
+			g.addEdge(src, dest);
+		}
+		g.printGraph();
+	}
+
+}
